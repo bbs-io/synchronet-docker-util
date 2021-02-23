4ff1e8197058fdf30864f318ebc17f778a60d473
Michael J. Ryan <tracker1@gmail.com>
2021-02-22 18:22:33 -0700

    fix publish readme

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -10,10 +10,11 @@ pkg.scripts = undefined;
 pkg.bin.synchronet = "cli.js";
 pkg.main = "main.js";
 
+// package.json and readme
 fs.writeFileSync(`${dist}/package.json`, JSON.stringify(pkg, null, 2), "utf8");
+shell.cp(`${__dirname}/../README.md`, `${dist}/README.md`);
 
 // resources in dist/_res
 shell.rm("-rf", `${dist}/_res/*`);
 shell.mkdir("-p", `${dist}/_res/`);
 shell.cp("-R", `${src}/_res/*`, `${dist}/_res/`);
-shell.cp(`${src}/README.md`, `${dist}/README.md`);
